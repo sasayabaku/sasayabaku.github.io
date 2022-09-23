c1fec9445b1199b63bc731834e8c8745f89f43f8
sasayabaku <sasayabaku@yahoo.co.jp>
2022-09-23 10:50:24 +0900

    Add contents

---
+++ pages/[...slug].vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="sentence">
+    <ContentDoc />
+  </main>
+</template>
+
+
+<style scoped>
+.sentence {
+  width: 75%;
+  margin: 0 auto;
+  line-height: 200%;
+}
+</style>
